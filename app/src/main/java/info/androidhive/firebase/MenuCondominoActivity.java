e04e482a4eda39c40e31feab0aee2ca2fb5ceb26
FranklynSabat <franklynsabat@gmail.com>
2018-05-02 13:14:40 -0300

    tcc versao 1.5
    versao atual

---
+++ app/src/main/java/info/androidhive/firebase/MenuCondominoActivity.java
@@ -0,0 +1,70 @@
+package info.androidhive.firebase;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuCondominoActivity extends AppCompatActivity {
+
+    private Button btnSair, btnAvaliar, btnDuvidas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_condomino);
+
+
+        FloatingActionButton btnDuvidas  = findViewById(R.id.btn_duvidas);
+        FloatingActionButton btnAvaliar =  findViewById(R.id.btn_avaliar);
+        btnSair =   findViewById(R.id.btn_sair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                finish();
+
+            }
+
+        });
+
+
+
+        btnAvaliar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(),AvaliacaoActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        btnDuvidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(getApplicationContext(),ChatBotMain.class);
+                startActivity(intent2);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
---
+++ app/src/main/java/info/androidhive/firebase/SplashActivity.java
@@ -0,0 +1,20 @@
+package info.androidhive.firebase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Intent intent = new Intent(this, ChatBotMain.class);
+        startActivity(intent);
+
+
+    }
+}
